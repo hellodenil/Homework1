3cefc6a2c77bd351fc4d32a2e9cc605b6661e418
Danil Mamin <denil2006060506@gmail.com>
2024-10-13 15:16:32 +0300

    Add Hello message in dif languages

---
+++ hello.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var lng := ReadString('Введите язык: ');
+  if lng = 'Русский' then
+    Print('Привет')
+  else if lng = 'Английский' then
+    Print('Hello')
+  else if lng = 'Китайский' then
+    Print('你好');
+end.
